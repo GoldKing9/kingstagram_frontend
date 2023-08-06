78e7b9d7a10186a4bf42185bba925b288b960c24
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2023-08-06 22:52:23 +0900

    회원가입 페이지 퍼블리싱 완료
    회원가입 페이지 퍼블리싱 완료
    
    #7
    
    1. 기능구현 들어가야함. 로그인페이지 라우팅주소 넣어줘야 함.
    2. 회원가입 조건들 검증에 관한 구현 및 에러메세지 출력 구현 필요함

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './components/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Signup from './pages/Signup';
 
 function App() {
 
     return (
         <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
+                    <Route path="/signup" element={<Signup />} />
                 </Routes>
         </Router>
     )
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import kingstagramLogo from '../assets/Kingstagram.svg';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Container from '@mui/system/Container';
+import Typography from '@mui/material/Typography';
+
+const Signup = () => {
+  return (
+    <Container fixed>
+          <Box
+            sx = {{ border: '1px solid rgb(219, 219, 219)', 
+            padding: '20px', 
+            height: '500px',
+            mb: '20px',
+            borderRadius: '5px',
+            }}
+            component = "div"
+            width = '350px' 
+          >
+            <Typography
+             sx={{ mt: '20px'}}
+             color = 'gray'
+             textAlign = 'center'
+            >
+              <img 
+                src = {kingstagramLogo} 
+                alt = "Kingstagram"
+                width = "250px" 
+              />
+              <Box 
+                component = 'div'
+                sx={{ mt: '5px' }}
+              >
+                친구들의 사진과 동영상을 보려면 가입하세요.
+              </Box>
+            </Typography> 
+            <TextField
+            required
+            fullWidth
+            id = 'email'
+            label = '이메일'
+            name = 'email'
+            autoComplete = 'email'
+            autoFocus
+            sx = {{ mt: "30px" }}
+            />
+            <TextField
+            required
+            fullWidth
+            id = 'name'
+            label = '성명'
+            name = 'name'
+            autoFocus
+            sx = {{ mt: "10px" }}
+            />
+            <TextField
+            required
+            fullWidth
+            id = 'nickname'
+            label = '사용자 이름'
+            name = 'nickname'
+            autoFocus
+            sx = {{ mt: "10px" }}
+            />
+            <TextField
+            required
+            fullWidth
+            id = 'password'
+            label = '비밀번호'
+            name = 'password'
+            autoComplete = 'current-password'
+            type = 'password'
+            sx = {{ mt: "10px", mb: "50px" }}
+            />
+            <Button
+              type = 'submit'
+              fullWidth
+              variant='contained'
+              sx={{ fontSize: '16px' }}
+            >
+              가입
+            </Button>  
+          </Box>
+          <Box
+            component = "div"
+            sx = {{ border: '1px solid rgb(219, 219, 219)',
+            borderRadius: '5px'
+            }}
+            height = '70px'
+            fontSize = '20px'
+            textAlign = 'center'
+            lineHeight='70px'
+          >
+            계정이 있으신가요?  
+            <Link 
+              href ='/'
+              style={{ textDecoration: 'none' }}
+            >
+              &nbsp;&nbsp;로그인
+            </Link>
+          </Box>
+          <Box component = 'footer'>
+           <Typography>
+           <img 
+              src={kingstagramLogo} 
+              alt="Kingstagram_logo"
+              height = '100px'
+              width= '100px' 
+            />
+            </Typography> 
+          </Box>
+    </Container>
+  );
+}
+
+export default Signup;
