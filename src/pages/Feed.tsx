0a38fce93970820bbd500502ac2459447c4372c7
GHeeJeon <jhjeon_99@naver.com>
2023-08-09 03:36:59 +0900

    [feature/navigation] merge-restore files

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,13 @@
+import NavigationBar from "../components/NavigationBar.tsx"
+
+
+
+const Feed = () => {
+    return (
+        <>
+            <NavigationBar></NavigationBar>
+        <h1>Feed Page</h1>
+    </>);
+};
+
+export default Feed;
