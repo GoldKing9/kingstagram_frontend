aae03334c4ed62f5ab3f053f023e6db04a5459ce
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2023-08-10 08:01:01 +0900

    프로필 퍼블리싱 1차완료
    프로필 페이지 퍼블리싱 1차완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './components/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Feed from './pages/Feed';
 import Myprofile from './pages/Myprofile';
+
 
 function App() {
 
     return (
         <Router>
                 <Routes>
-                    <Route path= "/feed" element={<Feed />} />
-                    <Route path= "/myprofile" element={<Myprofile />} />
+                    <Route path="/myprofile" element={<Myprofile />} />
                 </Routes>
         </Router>
     )
