af75f8bf86dfaad29ab2d6f148ab3656728b0d39
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2023-08-12 19:11:57 +0900

    회원가입 유효성 검증 구현
    회원가입 유효성 검증 구현 완료
    
    보완할점
    
    1.  isValid 상태에 따라 button 비활성화 / 활성화 구현
    
    공부할점
    useRef(), useEffect(), debounce

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import kingstagramLogo from '../assets/Kingstagram.png';
@@ -7,7 +7,150 @@ import Link from '@mui/material/Link';
 import Container from '@mui/system/Container';
 import Typography from '@mui/material/Typography';
 
+interface form {
+  email: null | string;
+  password: null | string;
+  name: null | string;
+  nickname: null | string;
+}
+
+interface isValidForm {
+  isValidEmail: boolean;
+  isValidPassword: boolean;
+  isValidName: boolean;
+  isValidNickname: boolean;
+}
+
+const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>): ReturnType<T> => {
+    let result: any;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      result = fn(...args);
+    }, delay);
+    return result;
+  };
+};
+
+// const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
+//   let timeout: ReturnType<typeof setTimeout> | null = null;
+//   return (...args: Parameters<T>): Promise<ReturnType<T>> => {
+//     return new Promise((resolve) => {
+//       if (timeout) clearTimeout(timeout);
+//       timeout = setTimeout(() => {
+//         resolve(fn(...args));
+//       }, delay);
+//     });
+//   };
+// };
+
 const Signup = () => {
+  
+  const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
+  const passwordRegEx = /^[A-Za-z0-9!@#$%^&*]{8,16}$/;
+  const nameRegEx = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,5}$/;
+  const nicknameRegEx = /^([_]?[a-z0-9]){4,16}$/;
+  
+  const [form, setForm] = useState<form>({
+    email: null,
+    password: null,
+    name: null,
+    nickname: null
+  });
+
+  const [isValid, setIsValid] = useState<isValidForm>({
+    isValidEmail: false,
+    isValidPassword: false,
+    isValidName: false,
+    isValidNickname: false
+  })
+  
+  // const buttonRef = useRef<HTMLButtonElement | null> (null) 
+  // const emailRef = useRef<HTMLInputElement>(null);
+  // const passwordRef = useRef<HTMLInputElement>(null);
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const nicknamelRef = useRef<HTMLInputElement>(null);
+
+  // useEffect(() => {
+  //   if(buttonRef.current && emailRef.current && passwordRef.current && nameRef.current && passwordRef.current) {
+  //     if(emailRef.current.children[1].color) {
+  //       tmpRef.current.disabled = true;
+  //     } else {
+  //       tmpRef.current.disabled = false;
+  //     }
+  //   }
+  // }, [isValid.isValidEmail, isValid.isValidPassword, isValid.isValidName, isValid.isValidNickname]);
+
+  const isValidEmail = debounce((e) => {
+      
+      const currentEmail = e.target.value;
+    
+      setForm({...form, email: currentEmail});
+
+      console.log(currentEmail);
+      
+      if (!emailRegEx.test(currentEmail)) {
+        setIsValid({...isValid, isValidEmail: false});
+        console.log("이메일 형식에 맞게 작성해주세요");
+        alert("이메일 형식에 맞게 작성해주세요");
+      } else {
+        setIsValid({...isValid, isValidEmail: true});
+      }
+      console.log(isValid.isValidEmail)
+  }, 1000);
+
+
+    
+  const isValidPassword = debounce((e) => {
+
+    const currentPassword = e.target.value;
+    
+    setForm({...form, email: currentPassword});
+
+    console.log(currentPassword);
+
+    if (!passwordRegEx.test(currentPassword)) {
+      alert("비밀번호 형식(소문자 or 대문자,숫자,!@#$%^&* 포함 8자에서 16자)에 맞게 작성해주세요");
+    } else {
+      setIsValid({...isValid, isValidPassword: true});
+    }
+    console.log(isValid.isValidPassword);
+  }, 1000)
+
+  const isValidName = debounce((e) => {
+    
+    const currentName = e.target.value;
+    
+    setForm({...form, email: currentName});
+
+    console.log(currentName);
+
+    if (!nameRegEx.test(currentName)) {
+      alert("이름 형식(한글로 이루어진 2자~5자)에 맞게 작성해주세요");
+    } else {
+      setIsValid({...isValid, isValidName: true});
+    }
+    console.log(isValid.isValidName);
+  }, 1000)
+
+  const isValidNickname = debounce((e) => {
+
+    const currentNickname = e.target.value;
+    
+    setForm({...form, email: currentNickname});
+
+    console.log(currentNickname);
+
+    if (!nicknameRegEx.test(currentNickname)) {
+      alert("사용자 이름 형식(소문자,숫자,_(생략가능) 포함 4자~16자)에 맞게 작성해주세요");
+    } else {
+      setIsValid({...isValid, isValidNickname: true});
+    }
+    console.log(isValid.isValidNickname);
+  }, 1000)
+  
   return (
     <Container fixed>
           <Box
@@ -38,35 +181,40 @@ const Signup = () => {
               </Box>
             </Typography> 
             <TextField
-            required
             fullWidth
             id = 'email'
             label = '이메일'
             name = 'email'
             autoComplete = 'email'
-            autoFocus
             sx = {{ mt: "30px" }}
+            // value = {form.email}
+            onChange={isValidEmail}
+            color = {isValid.isValidEmail ? 'success' : 'error'}
+            // ref={emailRef}
             />
             <TextField
-            required
             fullWidth
             id = 'name'
             label = '성명'
             name = 'name'
-            autoFocus
             sx = {{ mt: "10px" }}
+            // value = {form.name}
+            onChange={isValidName}
+            // ref={nameRef}
+            color = {isValid.isValidName ? 'success' : 'error'}
             />
             <TextField
-            required
             fullWidth
             id = 'nickname'
             label = '사용자 이름'
             name = 'nickname'
-            autoFocus
             sx = {{ mt: "10px" }}
+            // value = {form.nickname}
+            onChange={isValidNickname}
+            color = {isValid.isValidNickname ? 'success' : 'error'}
+            // ref={nicknamelRef}
             />
             <TextField
-            required
             fullWidth
             id = 'password'
             label = '비밀번호'
@@ -74,12 +222,18 @@ const Signup = () => {
             autoComplete = 'current-password'
             type = 'password'
             sx = {{ mt: "10px", mb: "50px" }}
+            // value = {form.password}
+            onChange={isValidPassword}
+            color = {isValid.isValidPassword ? 'success' : 'error'}
+            // ref={passwordRef}
             />
             <Button
               type = 'submit'
               fullWidth
               variant='contained'
-              sx={{ fontSize: '16px' }}
+              sx={{ fontSize: '16px', bgcolor: '#68b5fa', borderRadius: '10px'  }}
+              // disabled 
+              // ref = {buttonRef}
             >
               가입
             </Button>  
@@ -90,7 +244,7 @@ const Signup = () => {
             borderRadius: '5px'
             }}
             height = '70px'
-            fontSize = '20px'
+            fontSize = '16px'
             textAlign = 'center'
             lineHeight='70px'
           >
@@ -102,13 +256,16 @@ const Signup = () => {
               &nbsp;&nbsp;로그인
             </Link>
           </Box>
-          <Box component = 'footer'>
+          <Box 
+            component = 'footer'
+            sx={{ mt: '100px' }} 
+          >
            <Typography>
            <img 
               src={kingstagramLogo} 
               alt="Kingstagram_logo"
-              height = '100px'
-              width= '100px' 
+              height = '20px'
+              width= '100px'
             />
             </Typography> 
           </Box>
