1bb6db10ec18ba7e20350a7cd42fc0bfd4535e00
GHeeJeon <jhjeon_99@naver.com>
2023-08-13 23:52:07 +0900

    [feature/feed] apply https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mkcert()
+  ],
   optimizeDeps: {
     exclude: ['js-big-decimal']
+  },
+  server: {
+    https: true
   }
 })
